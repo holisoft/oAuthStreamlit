75094adb2c936239424f212f799ff385fed6e644
holisoft <fdefazio@holisoft.it>
2025-06-10 17:55:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,21 @@
-# app.py
 import streamlit as st
 import login
 
-# Deve essere la PRIMA chiamata di Streamlit
-st.set_page_config(page_title="Dashboard HoliSoft", layout="wide")
+st.set_page_config(page_title="Dashboard Listini HoliSoft", layout="wide")
 
-# Debug iniziale
 st.write("✅ App avviata.")
 st.write("📦 Session state:", dict(st.session_state))
-st.write("🌐 Host corrente:", st.runtime.scriptrunner.get_request_context().host)
 st.write("📦 Query params:", st.query_params)
 
-# **Flusso di autenticazione**
 if not st.session_state.get("authenticated"):
     if "code" in st.query_params:
-        # Sei rientrato dal redirect Azure
         login.handle_callback()
     else:
-        # Mostra il link di login
         login.show()
     st.stop()
 
-# **Qui sei autenticato**
 st.success("✅ Login effettuato con successo!")
+st.json(st.session_state["token"])
 
 # Visualizza il token per conferma
 st.subheader("🔑 Dati del token")
