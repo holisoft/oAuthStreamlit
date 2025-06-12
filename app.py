f7ff2c4ba07b07f4f8e868d25b59cb4a39161f53
holisoft <68947791+holisoft@users.noreply.github.com>
2025-06-12 11:18:30 +0200

    Fix login order

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ st.title("Esempio di Login Streamlit")
 
 # Mostra la form di login
 name, authentication_status, username = authenticator.login(
-    "main",
-    "Login"
+    "Login",
+    "main"
 )
 
 # Gestione stato autenticazione
